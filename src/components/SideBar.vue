183bbaff794888a8298d2d50c29e5bd55b914167
Falcon <falconhawl@protonmail.com>
2019-11-27 00:57:51 +0900

    prototype implemented

---
+++ src/components/SideBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <aside class="menu side-bar scrollbar">
+    <p class="menu-label">Apple Music</p>
+    <ul class="menu-list">
+      <li>
+        <ul>
+          <li><a class="is-active">Members</a></li>
+          <li><a>Plugins</a></li>
+          <li><a>Add a member</a></li>
+        </ul>
+      </li>
+    </ul>
+
+    <p class="menu-label">Library</p>
+    <ul class="menu-list">
+      <li>
+        <ul>
+          <li><a>Members</a></li>
+          <li><a>Plugins</a></li>
+          <li><a>Add a member</a></li>
+        </ul>
+      </li>
+    </ul>
+
+    <p class="menu-label">Playlist</p>
+    <ul class="menu-list">
+      <li>
+        <ul>
+          <li><a>Members</a></li>
+          <li><a>Plugins</a></li>
+          <li><a>Add a member</a></li>
+        </ul>
+      </li>
+    </ul>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'side-bar',
+};
+</script>
+
+<style scoped></style>
--- src/store/music.js
+++ src/store/music.js
@@ -1,4 +1,26 @@
-console.log('test from music');
+/* import keys from '../../apiKeys';
+
+// eslint-disable-next-line no-undef
+ MusicKit.configure({
+  developerToken: keys.appleMusic.developerToken,
+  app: {
+    name: 'My Cool Web App',
+    build: '1978.4.1',
+  },
+});
+
+// eslint-disable-next-line no-undef
+const music = MusicKit.getInstance();
+
+// Set the playback queue to a specific album, and play
+music.setQueue({
+  album: '1025210938',
+});
+
+console.log(music);
+
+music.play();
+ */
 
 const music = {};
 
