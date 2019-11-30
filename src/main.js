2b2a126701cd8acb08df94d9f1cf5785d1764734
Falcon <falconhawl@protonmail.com>
2019-12-01 00:11:06 +0900

    Added basic route config and got apple music api to work. Gotta play a bit more with apple music api to a get a hang of it.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import 'bulma/css/bulma.min.css';
+// import 'bulma/css/bulma.min.css';
 import '@/assets/main.scss';
 
 Vue.config.productionTip = false;
--- src/views/Albums.vue
+++ src/views/Albums.vue
@@ -1,11 +1,36 @@
 <template>
-  <p>album lists</p>
+  <div class="albums-page scrollWrapper">
+    <thumbnail-and-title
+      class="margin"
+      v-for="album in albums"
+      :key="album.id"
+      :id="album.id"
+      :size="'small'"
+      :artist="album.artist"
+      :title="album.title"
+      :artwork="setImageSize(album.artwork, 150, 150)"
+    />
+  </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+import ThumbnailAndTitle from '@/components/ThumbnailAndTitle.vue';
+
 export default {
-  name: 'Albums',
+  name: 'albums-page',
+  components: {
+    ThumbnailAndTitle,
+  },
+  methods: {
+    setImageSize(url, width, height) {
+      return url.replace('{w}', width).replace('{h}', height);
+    },
+  },
+  computed: {
+    ...mapGetters('music', {
+      albums: 'getLibraryAlbums',
+    }),
+  },
 };
 </script>
-
-<style scoped></style>
