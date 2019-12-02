8222cf04a0392443214daf4abf5f28194cfee8fc
Falcon <falconhawl@protonmail.com>
2019-12-02 15:12:42 +0900

    changed to dark theme, look way better now

--- src/router/routerConstants.js
+++ src/router/routerConstants.js
@@ -2,6 +2,7 @@ const routes = [
   {
     routeName: '/apple',
     category: 'APPLE MUSIC',
+    icon: 'music',
     labels: [
       { name: 'For You', routeName: '/library/for-you' },
       { name: 'Browse', routeName: '/library/browse' },
@@ -10,11 +11,13 @@ const routes = [
   {
     routeName: '/library',
     category: 'LIBRARY',
+    icon: 'save',
     labels: [
       { name: 'Recently Added', routeName: '/library/recently-added' },
       { name: 'Artists', routeName: '/library/my-artists' },
       { name: 'Albums', routeName: '/library/my-albums' },
       { name: 'Songs', routeName: '/library/songs' },
+      { name: 'Playlists', routeName: '/library/playlists' },
     ],
   },
 ];
