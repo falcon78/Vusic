c2564006c1effb033643c1033927dfa15baa9f7b
falconh78 <falconhawk@protonmail.com>
2019-12-15 15:54:26 +0900

    removed unnecessary parts

--- src/router/index.js
+++ src/router/index.js
@@ -86,17 +86,6 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes,
-  scrollBehavior(to, from, savedPosition) {
-    console.log('i was here ');
-    console.log(savedPosition);
-    if (savedPosition) {
-      return savedPosition;
-    }
-    if (to.hash) {
-      return { selector: to.hash };
-    }
-    return { x: 0, y: 0 };
-  },
 });
 
 export default router;
