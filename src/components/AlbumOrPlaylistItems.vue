30833fb8f5340ff8295bd3f9aec8c63cfad14dfe
falconh78@home-ubuntu <falconhawk@protonmail.com>
2019-12-28 18:22:15 +0900

    cleaned up AlbumOrPlaylistItems.vue component

--- src/components/AlbumOrPlaylistItems.vue
+++ src/components/AlbumOrPlaylistItems.vue
@@ -23,42 +23,26 @@ export default {
   mixins: [getSafeMixin, musicMixin],
   watch: {
     '$route.params.id': function() {
-      console.log(this.$route.params.id);
-      // this.$route.meta.album ? this.getAlbumInfo() : this.getPlaylistInfo();
-      this.item = null
       this.getItems();
     },
   },
   data() {
     return {
-      id: this.$route.params.id,
       item: null,
       isLibrary: this.getSafe(() => this.$route.meta.isLibrary, false),
     };
   },
   methods: {
-    getPlaylistInfo() {
-      this.getApi(this.isLibrary)
-        .playlist(this.id)
-        .then((playlist) => {
-          this.item = { ...playlist };
-          console.log(playlist);
-        });
-    },
-    async getAlbumInfo() {
-      const album = await this.getApi(this.isLibrary).album(this.id);
-      this.item = { ...album };
-    },
     async getItems() {
+      const id = this.$route.params.id;
       const music = this.isLibrary
         ? MusicKit.getInstance().api.library
         : MusicKit.getInstance().api;
-      this.item = await music[this.$route.meta.album ? 'album' : 'playlist'](this.id);
+      console.log(this.$route.params.id, this.$route.meta.album);
+      this.item = await music[this.$route.meta.album ? 'album' : 'playlist'](id);
     },
   },
   mounted() {
-    console.log('ooooooo');
-    //this.$route.meta.album ? this.getAlbumInfo() : this.getPlaylistInfo();
     this.getItems();
   },
 };
