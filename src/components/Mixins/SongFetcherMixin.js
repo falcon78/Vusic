9b6dde58702599abf9d9fd9307157a5fbc225b65
falconh78@home-ubuntu <falconhawk@protonmail.com>
2019-12-29 13:50:08 +0900

    prevent infinite loop and added error handling

--- src/components/Mixins/SongFetcherMixin.js
+++ src/components/Mixins/SongFetcherMixin.js
@@ -1,21 +1,29 @@
 const songFetcherMixin = {
   data() {
     return {
-      offset: 200,
+      offset: 0,
     };
   },
   methods: {
     async fetchAllItems(library = true, item = 'songs', options = null) {
       const music = library ? MusicKit.getInstance().api.library : MusicKit.getInstance().api;
       let fetchingData = true;
-      while (fetchingData) {
+      let loopCount = 0;
+      while (fetchingData || loopCount < 500) {
         try {
-          await music[item](options, { offset: 100 }).then((songs) => {
+          await music[item](options, { offset: this.offset }).then((songs) => {
             fetchingData = !!songs.length;
             this[item] = songs;
             this.offset += songs.length;
+            loopCount++;
           });
-        } catch (e) {}
+        } catch (e) {
+          this.$swal({
+            type: 'error',
+            title: 'An Error Occurred...',
+            text: e.message,
+          });
+        }
       }
     },
   },
