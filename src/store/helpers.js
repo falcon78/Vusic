ad7e857da02c778621bd94e7353aa37b32421752
falconh78@home-ubuntu <falconhawk@protonmail.com>
2019-12-29 13:52:48 +0900

    made debounce function a module

--- src/store/helpers.js
+++ src/store/helpers.js
@@ -7,21 +7,6 @@ const helpers = {
     }
   },
 
-  debounce(func, wait, immediate) {
-    let timeout;
-    return function() {
-      let context = this,
-        args = arguments;
-      let later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
-      };
-      let callNow = immediate && !timeout;
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-      if (callNow) func.apply(context, args);
-    };
-  },
   fakeArtwork:
     'https://is1-ssl.mzstatic.com/image/thumb/Features127/v4/75/f9/6f/75f96fa5-99ca-0854-3aae-8f76f5cb7fb5/source/200x200bb.jpeg',
   getUrl(artwork, size) {
@@ -42,5 +27,21 @@ const helpers = {
     return MusicKit.getInstance().api;
   },
 };
+function debounce(func, wait, immediate) {
+  let timeout;
+  return function() {
+    let context = this,
+      args = arguments;
+    let later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
 
+export { debounce };
 export default helpers;
