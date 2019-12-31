df1b19c1b7999b974f80b4a9e774582ce646add0
falconh78 <falconhawk@protonmail.com>
2019-12-31 22:43:58 +0900

    logo, and favicon changes

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,16 +1,21 @@
 <template>
-  <div class="login-container">
+  <div class="login-container scrollWrapper">
     <div>
-      <h2>Sign In to view your library</h2>
+      <h2 class="browse-page-category">You need to Sign In to view your library</h2>
     </div>
-    <div class="login-image">
-      <img src="../assets/Images/login.png" alt="" />
-    </div>
+
     <a @click="login" class="button"><span>Sign In</span></a>
     <div>
       <h2>Or just browse ...</h2>
     </div>
     <a @click="$router.push('/browse')" class="button"><span>Browse</span></a>
+    <div style="margin-top: 4em;"></div>
+    <div style="text-align: center; width: 70%">
+      <h4 class="browse-page-category">
+        This site uses Apple MusicKitJS API to authenticate users. You login info is handled
+        directly by Apple's API.
+      </h4>
+    </div>
   </div>
 </template>
 
